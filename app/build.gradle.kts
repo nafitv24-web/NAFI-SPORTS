c3d49a9337db5620d2a23aeeabf2255c42cb51c5
nafitv24-web <nafitv24@gmail.com>
2026-08-18 19:51:18 +0600

    Update versionName to 2.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 26
-    versionName = "2.5.2"
+    versionName = "2.5.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
